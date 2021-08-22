d65954e0c9ce3fdf57d65624b168d392095f0322
waleedsaleem309 <70096047+waleedsaleem309@users.noreply.github.com>
2021-08-22 05:03:12 +0500

    Add files via upload

---
+++ test1/js/script.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector('.navbar');
+
+
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
